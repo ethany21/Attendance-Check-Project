14ab0834428c23ae13f32b3fda29115c14b900cc
ethany21 <imwoodam@hanmail.net>
2021-03-07 18:16:06 +0900

    Temp

---
+++ src/main/java/com/github/project/attendancecheck/controller/MyPageController.java
@@ -0,0 +1,45 @@
+package com.github.project.attendancecheck.controller;
+
+import com.github.project.attendancecheck.service.interfaces.ClassService;
+import com.github.project.attendancecheck.service.interfaces.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/myPage")
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final StudentService studentService;
+    private final ClassService classService;
+
+    @GetMapping({"/", ""})
+    public String myPage(Model model, Principal principal){
+
+
+        return null;
+    }
+
+    @GetMapping("/myInfo")
+    public String myInfo(){
+        return null;
+    }
+
+    @GetMapping("/classList")
+    public String classList(){
+
+        return null;
+
+    }
+
+    @GetMapping("/myFee")
+    public String myFee(){
+        return null;
+    }
+
+}
