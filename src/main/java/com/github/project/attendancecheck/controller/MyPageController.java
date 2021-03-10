499efb0f60e95952be1536f73f144fdff9dc303f
ethany21 <imwoodam@hanmail.net>
2021-03-10 13:40:28 +0900

    Closes #3, closes #5, closes #12, closes #16; Implemented Admin page, My page

--- src/main/java/com/github/project/attendancecheck/controller/MyPageController.java
+++ src/main/java/com/github/project/attendancecheck/controller/MyPageController.java
@@ -1,6 +1,10 @@
 package com.github.project.attendancecheck.controller;
 
+import com.github.project.attendancecheck.model.Attendance;
+import com.github.project.attendancecheck.model.Student;
+import com.github.project.attendancecheck.service.interfaces.AttendanceService;
 import com.github.project.attendancecheck.service.interfaces.ClassService;
+import com.github.project.attendancecheck.service.interfaces.PaidFeeService;
 import com.github.project.attendancecheck.service.interfaces.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/myPage")
@@ -17,32 +22,66 @@ public class MyPageController {
 
     private final StudentService studentService;
     private final ClassService classService;
+    private final PaidFeeService paidFeeService;
+    private final AttendanceService attendanceService;
 
     @GetMapping({"/", ""})
     public String myPage(Model model, Principal principal){
 
+        Student student = studentService.findByUsername(principal.getName());
+
+        model.addAttribute("student", student);
 
         return "MyPage/myPage";
     }
 
     @GetMapping("/myInfo")
-    public String myInfo(){
+    public String myInfo(Model model, Principal principal){
 
-        return null;
+        Student student = studentService.findByUsername(principal.getName());
+
+        model.addAttribute("details", student);
+
+        return "MyPage/myInfo";
 
     }
 
     @GetMapping("/classList")
-    public String classList(){
+    public String classList(Model model, Principal principal){
 
-        return null;
+        long id = studentService.findByUsername(principal.getName()).getId();
+
+        List<Attendance> attendances = attendanceService.findAllByStudent_Id(id);
+
+        model.addAttribute("attendances", attendances);
+
+        return "MyPage/classList";
 
     }
 
     @GetMapping("/myFee")
-    public String myFee(){
+    public String myFee(Model model, Principal principal){
 
-        return null;
+        long id = studentService.findByUsername(principal.getName()).getId();
+
+        List<Attendance> attendances = attendanceService.findAllByStudent_Id(id);
+
+        int sumFee = attendanceService.sumPenaltyFee(id);
+
+        int paidFee = paidFeeService.sumPaidFeeByStudent(id);
+
+        int restPayment = attendanceService.restPayment(id);
+
+        model.addAttribute("attendances", attendances);
+
+        model.addAttribute("sumPenaltyFee", sumFee);
+
+        model.addAttribute("paidFee", paidFee);
+
+        model.addAttribute("restPayment", restPayment);
+
+
+        return "MyPage/myFee";
 
     }
 
