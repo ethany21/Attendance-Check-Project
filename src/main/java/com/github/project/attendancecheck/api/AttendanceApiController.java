e2170c9738684c9638c857050294697b6e98374f
ethany21 <imwoodam@hanmail.net>
2021-05-11 23:59:08 +0900

    Added comment on RestController Class

--- src/main/java/com/github/project/attendancecheck/api/AttendanceApiController.java
+++ src/main/java/com/github/project/attendancecheck/api/AttendanceApiController.java
@@ -18,16 +18,20 @@ public class AttendanceApiController {
     private final AttendanceService attendanceService;
     private final ClassService classService;
 
+    /**출석 체크해야 할 수업 일자를 리스트로 열거*/
     @GetMapping("/attendances")
     public List<Class> showClasses(){
         List<Class> classes = classService.findAll();
         return classes;
     }
+
+    /**수업 일자를 선택하면, 학생들의 수 만큼 attendance 객체를 view로 전송*/
     @GetMapping("/checkAttendances")
     public AttendanceWrapper createAttendances(@RequestParam("classId") Long id){
         return attendanceService.createAttendances(new AttendanceWrapper(), id);
     }
 
+    /**view 쪽에서 저장을 누르면 attendance 객체 연관관계 매핑 후 저장*/
     @PostMapping("/saveAttendances")
     public void saveAttendances(@ModelAttribute AttendanceWrapper attendanceWrapper, @RequestParam Long classId){
         attendanceService.setAttendancesDate(attendanceWrapper, classId);
